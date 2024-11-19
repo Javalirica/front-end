77c372b1d25058a52f96f3af7b969d235c9065e4
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2024-11-18 22:37:03 -0300

    Scripts e HTML
    Scripts e HTML

--- front/interacao/script.js
+++ front/interacao/script.js
@@ -6,6 +6,8 @@ function login(){
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
+    getLogin(email,password)
+
     if(!email || !password){
         alert("Por favor, preencha todos os campos")
         return;
@@ -14,8 +16,59 @@ function login(){
     if (email == predefinidoEmail01  && password == predefinidosenha01){
         alert("Login efetuado com sucesso!")
         window.location.href = "home.html"
+
+        chamaTelaHome()
     }
     else {
         alert("Email ou senha incorretos!")
     }
+    
+    //se ternotar true chama home 
+    
+
 }
+
+function chamaTelaHome(){
+     window.location.href = "home.html"
+}
+function chamaTelaControle(){
+    window.location.href = "controle.html"
+}
+function chamaTelaCadastrarLeitores(){
+    window.location.href = "cadastarr-leitores.html"
+}
+function chamaTelaTodosOsLivros(){
+    window.location.href = "todososlivros.html"
+}
+
+
+
+
+function getLogin(email,password){
+
+
+
+
+}
+
+function buscaLivro(){
+
+
+
+
+}
+
+function cadastraUsario(){
+
+
+
+
+}
+
+
+
+
+
+
+
+
