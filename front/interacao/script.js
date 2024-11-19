96fd3b1b32ced2344bd02311dad64214075e34a7
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2024-11-19 20:07:41 -0300

    teste commit
    teste commit

--- front/interacao/script.js
+++ front/interacao/script.js
@@ -28,6 +28,79 @@ function login(){
 
 }
 
+function buscaLivro(){
+
+    //const codLivro = document.getElementById("code").value.trim();
+    //const nomeLivro = document.getElementById("nome").value.trim();
+    //const dataLivro = document.getElementById("date").value.trim();
+    //const situacaoLivro = document.getElementById("status").value.trim();
+
+
+   // if(codLivro != ''){
+
+    //}
+    
+   // else if(){
+        
+    //}
+    
+   // else if(){
+        
+    //}
+   // else if(){
+        
+   // }
+    
+    //else (){
+        
+   // }
+
+}
+
+function cadastraUsario(){
+
+    var nomeUsuario = document.getElementById("nome").value.trim();
+    var emailUsuario = document.getElementById("email").value.trim();
+    var senhaUsuario = document.getElementById("senha");
+    var valisaSenhaUsuario = document.getElementById("validaSenha");
+    var tipoUsuario = document.getElementById("tipoUsuario").value.trim();
+
+    validaCadastroUsuario(nomeUsuario,emailUsuario,senhaUsuario,valisaSenhaUsuario)
+
+    //fazer chamada da api 
+
+}
+
+function validaCadastroUsuario(nomeUsuario,emailUsuario,senhaUsuario,valisaSenhaUsuario){
+
+    if (nomeUsuario === '') {
+        console.log("O campo 'Nome' é obrigatório. Por favor, preencha-o.");
+    }
+    
+    if (emailUsuario === '') {
+        console.log("O campo 'E-mail' é obrigatório. Por favor, preencha-o.");
+    }
+    
+    if (senhaUsuario === '') {
+        console.log("O campo 'Senha' é obrigatório. Por favor, preencha-o.");
+    }
+    
+    if (valisaSenhaUsuario === '') {
+        console.log("O campo 'Confirmação de Senha' é obrigatório. Por favor, preencha-o.");
+    }
+    
+    if (valisaSenhaUsuario !== senhaUsuario) {
+        console.log("As senhas não coincidem. Por favor, verifique os campos de senha.");
+    }
+
+
+
+}
+
+
+
+
+
 function chamaTelaHome(){
      window.location.href = "home.html"
 }
@@ -40,30 +113,12 @@ function chamaTelaCadastrarLeitores(){
 function chamaTelaTodosOsLivros(){
     window.location.href = "todososlivros.html"
 }
-
-
-
-
+function chamaTelaLivrosEmprestados(){
+    window.location.href = "livrosemprestados.html"
+}
 function getLogin(email,password){
-
-
-
-
 }
 
-function buscaLivro(){
-
-
-
-
-}
-
-function cadastraUsario(){
-
-
-
-
-}
 
 
 
