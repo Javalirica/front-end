7b441b6980255ab8edcc71384ba34af01aae71e9
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2024-11-20 21:32:28 -0300

    refactor: Simplificação e estilização do html
    refactor: Simplificação e estilização do html

--- front/interacao/script.js
+++ front/interacao/script.js
@@ -120,15 +120,24 @@ function chamaTelaControle(){
 function chamaTelaCadastrarLeitores(){
     window.location.href = "cadastarr-leitores.html"
 }
+
 function chamaTelaCasdastraLivro(){
     //window.location.href = "buscaLivros.html"
 }
 function chamaTelaLivrosEmprestados(){
     window.location.href = "locacoes.html"
 }
+
 function cadastraUsario(){
     window.location.href = "novouser.html"
 }
+function alteraSenha(){
+    window.location.href = "alterarSenha.html"
+}
+function buscaUsuario(){
+    window.location.href = "buscaUsuarios.html"
+}
+
 function getLogin(email,password){
 }
 
