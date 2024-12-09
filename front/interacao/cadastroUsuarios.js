f246b0ea110b646111d93090523cc414d9dd5f8c
O Pedro Rodrigues <98363013+prod01@users.noreply.github.com>
2024-12-09 20:23:17 -0300

    Feat: Tela e Script novo usuario

--- front/interacao/cadastroUsuarios.js
+++ front/interacao/cadastroUsuarios.js
@@ -1,53 +1,36 @@
 function cadastraUsario() {
-  
+    console.log('Erro ao criar o gerenciador:');
     var nomeUsuario = document.getElementById("nome").value;
     var emailUsuario = document.getElementById("email").value;
     var senhaUsuario = document.getElementById("senha").value;
-    var valisaSenhaUsuario = document.getElementById("validaSenha").value;
     var tipoUsuario = document.getElementById("tipoUsuario").value;
 
-
-    // Validação de campos
-    if (!validaCadastroUsuario(nomeUsuario, emailUsuario, senhaUsuario, valisaSenhaUsuario)) {
-        return; // Se a validação falhar, não continua com o cadastro
-    }
-
+   
+    console.log('Erro ao criar o gerenciador:');
+        if (nomeUsuario == '') {
+            return("O campo 'Nome' é obrigatório. Por favor, preencha-o.");
+           
+        }
+    
+        if (emailUsuario == '') {
+            return("O campo 'E-mail' é obrigatório. Por favor, preencha-o.");
+           
+        }
+    
+        if (senhaUsuario == '') {
+            return("O campo 'Senha' é obrigatório. Por favor, preencha-o.");
+           
+        }
+    
+   
     // Fazer a chamada da API para cadastro, passando os dados do novo usuário
     createManager(nomeUsuario, tipoUsuario, emailUsuario, senhaUsuario);
 }
 
-function validaCadastroUsuario(nomeUsuario, emailUsuario, senhaUsuario, valisaSenhaUsuario) {
-    let isValid = true;
 
-    if (nomeUsuario === '') {
-        console.log("O campo 'Nome' é obrigatório. Por favor, preencha-o.");
-        isValid = false;
-    }
-
-    if (emailUsuario === '') {
-        console.log("O campo 'E-mail' é obrigatório. Por favor, preencha-o.");
-        isValid = false;
-    }
-
-    if (senhaUsuario === '') {
-        console.log("O campo 'Senha' é obrigatório. Por favor, preencha-o.");
-        isValid = false;
-    }
-
-    if (valisaSenhaUsuario === '') {
-        console.log("O campo 'Confirmação de Senha' é obrigatório. Por favor, preencha-o.");
-        isValid = false;
-    }
-
-    if (valisaSenhaUsuario !== senhaUsuario) {
-        console.log("As senhas não coincidem. Por favor, verifique os campos de senha.");
-        isValid = false;
-    }
-
-    return isValid;
-}
 
 async function createManager(nome, role, email, senha) {
+    console.log('Erro ao criar o gerenciador:');
     // Recuperando o token do localStorage
     const token = localStorage.getItem('authToken');
     
@@ -64,6 +47,7 @@ async function createManager(nome, role, email, senha) {
     };
 
     try {
+        console.log('Erro ao criar o gerenciador:');
         const response = await fetch('http://3.141.87.82:8080/gerenciador/novo', {
             method: 'POST',
             headers: {
@@ -72,7 +56,7 @@ async function createManager(nome, role, email, senha) {
             },
             body: JSON.stringify(managerData)
         });
-
+        console.log('Erro ao criar o gerenciador:');
         if (response.status === 201) {
             alert('Gerenciador criado com sucesso!');
             const createdManager = await response.json();
@@ -81,6 +65,10 @@ async function createManager(nome, role, email, senha) {
             alert('Erro ao tentar criar o gerenciador. Verifique os dados e tente novamente.');
         } else if (response.status === 409) {
             alert('O email informado já está cadastrado.');
+        }else {
+            console.log( response.status);
+           
+            
         }
     } catch (error) {
         console.error('Erro ao criar o gerenciador:', error);
